0b4a6d01a6ac6d7c90b243d7f3512f399f9c18a8
nn <yong881006@sina.com>
2024-05-16 14:55:47 +0800

    test

---
+++ src/main/java/com/dr/code/diff/dto/TestDto.java
@@ -0,0 +1,28 @@
+package com.dr.code.diff.dto;
+
+/**
+ * @Author: lyq
+ * @CreateTime: 2024-05-16  14:51
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class TestDto {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
